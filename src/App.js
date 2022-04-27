a8177c18afea28989fa2d7974875f85ad7533e62
Rafaqat Ahmad <irahilabassi@gmail.com>
2022-04-27 23:17:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [startCounter, setStartCounter] = useState(0);
+  const [endCounter, setEndCounter] = useState(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (endCounter) {
+        setCounter((preCount) => {
+          if (preCount < endCounter) {
+            return preCount + 1;
+          } else {
+            clearInterval(interval);
+            return preCount;
+          }
+        });
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [startCounter, endCounter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>counter:{counter}</h1>
+      start:
+      <input
+        onInput={(e) => {
+          setStartCounter(+e.target.value);
+          setCounter(+e.target.value);
+        }}
+        type="number"
+      />
+      end:
+      <input
+        onInput={(e) => {
+          setEndCounter(+e.target.value);
+          setCounter(startCounter);
+        }}
+        type="number"
+      />
     </div>
   );
 }
